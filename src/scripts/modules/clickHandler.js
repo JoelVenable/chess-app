462999ad6f7d7fc58673c0d2409eb0e7b6fa8e31
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-06 11:16:46 -0500

    Added click handler

--- src/scripts/modules/clickHandler.js
+++ src/scripts/modules/clickHandler.js
@@ -2,7 +2,7 @@ const chessboard = document.getElementById("board");
 
 chessboard.addEventListener("click", (event => {
   let turnColor = chessboard.getAttribute("data-turnColor");
-  let squareClicked = event.target
+  let squareClicked = event.target;
   //  get square clicked, finding the parent if the square-inner is target
   if (squareClicked.classList.contains("square-inner")) {
     squareClicked = squareClicked.parentNode;
